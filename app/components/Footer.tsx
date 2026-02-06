84eab002cf1ee672847a9fa596d0c637d10c85f1
Maria Luiza Dornelas <mariadornelasc12@gmail.com>
2026-02-05 21:07:57 -0300

    Ajusta design do footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,6 @@ export default function Footer() {
     return (
       <footer
         style={{
-          marginTop: "3rem",
           padding: "1rem 0",
           borderTop: "1px solid #ddd",
           textAlign: "center",
